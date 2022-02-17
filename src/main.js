6dae8c28ed9b25e85abe1d4ae358af6af1bc20e2
Piotr Moryto <pmoryto@pmoryto-mbpro.local>
2022-02-17 12:57:44 +0100

    declare CRUD functions

---
+++ src/main.js
@@ -0,0 +1,23 @@
+function getNotes() {
+
+}
+
+function saveNotes(notes) {
+
+}
+
+function createNoteElement(id, content) {
+
+}
+
+function addNote() {
+
+}
+
+function updateNote(id, newContent) {
+
+}
+
+function deleteNote(id, element) {
+    
+}
